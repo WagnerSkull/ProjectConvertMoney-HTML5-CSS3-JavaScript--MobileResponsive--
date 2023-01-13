4da545aef54b0136147809a9379d1a4e1f06574d
WagnerSkull <wagnerskull@hotmail.com>
2023-01-13 14:26:47 -0300

    Formatting the numbers

--- scripts.js
+++ scripts.js
@@ -5,20 +5,28 @@ const dolar = 5.2
 
 const convertValues = () => {
     //puxando o valor digitado no input valor
-const inputReais = document.getElementById(`input-real`).value
+    const inputReais = document.getElementById(`input-real`).value
 
-//Colocando o valor digitado no input no valor embaixo da bandeira
-const realValueText = document.getElementById(`real-value-text`)
+    //Colocando o valor digitado no input no valor embaixo da bandeira
+    const realValueText = document.getElementById(`real-value-text`)
 
-//Colocando o valor no dólar resultado.
-const currencyValueText = document.getElementById(`currency-value-text`)
+    //Colocando o valor no dólar resultado.
+    const currencyValueText = document.getElementById(`currency-value-text`)
 
-//Só ta jogando o valor do input no valor embaixo da bandeira.
-realValueText.innerHTML = inputReais
+    //Formatando o valor do Real, colocando em baixo da bandeira a forma correta. com pontos e virgúlas.
+    realValueText.innerHTML = new Intl.NumberFormat(`pt-BR`, {
+        style: `currency`,
+        currency: `BRL`,
+    }).format(inputReais);
 
-//Jogando o valor no dólar
-currencyValueText.innerHTML = inputReais / dolar
 
-}
+
+    //Formatando o valor do dólar pra fica correto os números.
+    currencyValueText.innerHTML = new Intl.NumberFormat(`en-US`, {
+        style: `currency`,
+        currency: `USD`,
+    }).format(inputReais / dolar);
+
+};
 //Ouvidor de evento, quando o botão e acionado, ele chama a função. 
-button.addEventListener(`click` , convertValues)
+button.addEventListener(`click`, convertValues)
