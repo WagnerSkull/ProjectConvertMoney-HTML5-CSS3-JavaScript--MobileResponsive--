255e3b331daf6ce8230fb22082f17620394e9fcf
WagnerSkull <wagnerskull@hotmail.com>
2023-01-14 07:14:58 -0300

    Finishing Project Javascript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const button = document.getElementById(`convert-button`)
 
+//Mapeando o Select para fazer as alterações de Dólar para EURO.
+const select = document.getElementById(`currency-select`)
+
+
 //Definição do valor do dólar
 const dolar = 5.2
 
@@ -17,7 +21,7 @@ const convertValues = () => {
     realValueText.innerHTML = new Intl.NumberFormat(`pt-BR`, {
         style: `currency`,
         currency: `BRL`,
-    }).format(inputReais);
+    }).format(inputReais)
 
 
 
@@ -26,7 +30,32 @@ const convertValues = () => {
         style: `currency`,
         currency: `USD`,
     }).format(inputReais / dolar);
-
 };
+
+//Função Change Currency, função responsavel por troca Dólar pra EURO.
+changeCurrency = () => {
+    const currencyName = document.getElementById(`currency-name`)
+//Const da troca da bandeira EUA - EURO
+const currencyImg = document.getElementById(`currency-img`)
+
+//O if esta trocando o nome embaixo da bandeira depois que e selecionado no SELECT.
+if(select.value === `US$ Dólar Americano`){
+    currencyName.innerHTML = `Dólar Americano`
+    //Trocando a img da Bandeira Euro pela Americana
+    currencyImg.src = "./img/EstadosUnidos.png"
+} 
+
+//O if esta trocando o nome embaixo da bandeira depois que e selecionado no SELECT.
+    if(select.value === `€ Euro`){
+        currencyName.innerHTML = `€ Euro`
+        //Trocando a img da Bandeira americana pela do EURO
+        currencyImg.src = "./img/Euro.png"
+    } 
+}
+
+
 //Ouvidor de evento, quando o botão e acionado, ele chama a função. 
 button.addEventListener(`click`, convertValues)
+
+//Ouvidor de evento do Select, nesse esta pegando o valor de change,Toda vez que alterar o valor ela chama a função.
+select.addEventListener(`change`, changeCurrency)
